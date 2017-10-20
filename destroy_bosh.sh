0970581b9137642e46a352365fd47f4a4ff4a393
datianshi <dsz0111@gmail.com>
2017-10-20 10:15:52 -0500

    make destroy_bosh.sh available

--- destroy_bosh.sh
+++ destroy_bosh.sh
@@ -3,6 +3,6 @@ set -e
 
 bosh-cli delete-env bosh-deployment/bosh.yml \
   --state ./${BOSH_WORK_DIR}/state.json \
-  -o bosh-deployment/${BOSH_WORK_DIR}/cpi.yml \
+  -o bosh-deployment/aws/cpi.yml \
   --vars-store ${BOSH_WORK_DIR}/creds.yml \
   --vars-file ./director.yml
--- destroy_env.sh
+++ destroy_env.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-set -ex
+set -e
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 TSTATE_FILE="${DIR}/terraform.tfstate"
@@ -8,6 +8,7 @@ TSTATE_FILE="${DIR}/terraform.tfstate"
 JUMPBOX_IP=$(terraform state show -state ${TSTATE_FILE} aws_instance.jumpbox | grep public_ip | tail -n 1 | awk '{print $3}')
 ssh -tt -o "StrictHostKeyChecking=no" -i ${PRIVATE_KEY_PATH} ubuntu@${JUMPBOX_IP} 'bash -es' <<ENDSSH
   cd management-vpc
+  git pull origin master
   source env.sh
   BOSH_WORK_DIR="../bosh_work" ./destroy_bosh.sh
   exit
