3c8a4ac9d4f7c7a7f31640c97bd8e45110bdc6b7
datianshi <dsz0111@gmail.com>
2017-10-20 16:20:56 -0500

    add team credentials

--- concourse/prepare_team.sh
+++ concourse/prepare_team.sh
@@ -1,8 +1,34 @@
+set -e
 
-ATC_PASSWORD=$1
-ATC_TARGET=$2
-PCF_ENV=$3
+#Needs Pass PCF ENV, TEAM USERNAME, TEAM PASSWORD
 
-fly -t aws login -k -c ${ATC_TARGET} -u atc -p ${ATC_PASSWORD}
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+TSTATE_FILE="${DIR}/terraform.tfstate"
 
-fly -t aws
+export VAULT_IP=$(bosh-cli int ./director.yml --path /vault_static_ips/0)
+export VAULT_ADDR=https://${VAULT_IP}:8200
+export VAULT_SKIP_VERIFY=true
+export ATC_PASSWORD=$(bosh-cli int ${BOSH_WORK_DIR}/bosh_work/concourse_creds.yml --path /admin_password)
+
+ATC_URL=$(terraform state show -state ${TSTATE_FILE} aws_elb.concourse | grep dns_name | awk '{print $3}')
+ATC_TARGET="https://${ATC_URL}"
+
+wget --no-check-certificate -O fly "https://${ATC_URL}/api/v1/cli?arch=amd64&platform=linux" && chmod +x fly
+
+./fly -t aws login -k -c ${ATC_TARGET} -u atc -p ${ATC_PASSWORD}
+./fly -t aws sync
+
+./fly -t aws set-team --team-name ${PCF_ENV} \
+    --basic-auth-username ${TEAM_USERNAME} \
+    --basic-auth-password ${TEAM_PASSWORD}
+
+for n in db_master_password db_app_usage_service_password db_autoscale_password db_diego_password db_notifications_password db_routing_password db_uaa_password db_ccdb_password db_accountdb_password db_networkpolicyserverdb_password db_nfsvolumedb_password db_silk_password db_locket_password
+  do
+  pass=$(openssl rand -base64 20)
+  vault write concourse/${PCF_ENV}/${n} value=${pass}
+done
+
+vault write concourse/${PCF_ENV}/pivnet_token value=${PIVNET_TOKEN}
+vault write concourse/${PCF_ENV}/aws_access_key value=${AWS_ACCESS_KEY}
+vault write concourse/${PCF_ENV}/aws_secret_key value=${AWS_SECRET_KEY}
+vault write concourse/${PCF_ENV}/opsman_password value=${OPSMAN_PASSWORD}
---
+++ create_team.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+
+
+# PCF_ENV=test TEAM_USERNAME=test TEAM_PASSWORD=test OPSMAN_PASSWORD=admin ./concourse/prepare_team.sh
+
+set -e
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+TSTATE_FILE="${DIR}/terraform.tfstate"
+
+JUMPBOX_IP=$(terraform state show -state ${TSTATE_FILE} aws_instance.jumpbox | grep public_ip | tail -n 1 | awk '{print $3}')
+ssh -tt -o "StrictHostKeyChecking=no" -i ${PRIVATE_KEY_PATH} ubuntu@${JUMPBOX_IP} PIVNET_TOKEN=${PIVNET_TOKEN} \
+PCF_ENV=${PCF_ENV} \
+TEAM_USERNAME=${TEAM_USERNAME} \
+TEAM_PASSWORD=${TEAM_PASSWORD} \
+AWS_ACCESS_KEY=${TF_VAR_aws_access_key} \
+AWS_SECRET_KEY=${TF_VAR_aws_secret_key} \
+OPSMAN_PASSWORD=${OPSMAN_PASSWORD}
+'bash -es' <<ENDSSH
+  cd management-vpc
+  git pull origin master
+  TEAM_USERNAME=${TEAM_USERNAME} TEAM_PASSWORD=${TEAM_PASSWORD}   AWS_ACCESS_KEY=${AWS_ACCESS_KEY} AWS_SECRET_KEY=${AWS_SECRET_KEY} OPSMAN_PASSWORD=${OPSMAN_PASSWORD} ./concourse/prepare_team.sh
+  exit
+ENDSSH
