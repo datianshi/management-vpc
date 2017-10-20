cd9f459116f64bc51230f11c9f14803009c2cb98
datianshi <dsz0111@gmail.com>
2017-10-20 10:38:39 -0500

    fix the IPs

--- destroy_bosh.sh
+++ destroy_bosh.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 set -e
 
+director=$(bosh-cli int ./director.yml --path /internal_ip)
+director_name=$(bosh-cli int ./director.yml --path /director_name)
+
+export BOSH_CLIENT=admin
+export BOSH_CLIENT_SECRET=$(bosh-cli int ${BOSH_WORK_DIR}/creds.yml --path /admin_password)
+
+bosh-cli -e ${director_name} -d vault delete-deployment
+bosh-cli -e ${director_name} -d concourse delete-deployment
+bosh-cli -e ${director_name} clean-up --all
+
 bosh-cli delete-env bosh-deployment/bosh.yml \
   --state ./${BOSH_WORK_DIR}/state.json \
   -o bosh-deployment/aws/cpi.yml \
