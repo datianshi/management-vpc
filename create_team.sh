54b4e00935624f0b8e1e6d7c88fe47ae4a9bffc0
datianshi <dsz0111@gmail.com>
2017-10-20 17:04:29 -0500

    delete env

--- create_team.sh
+++ create_team.sh
@@ -8,6 +8,8 @@ set -e
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 TSTATE_FILE="${DIR}/terraform.tfstate"
+source "${DIR}/env.sh"
+echo "${TF_VAR_aws_access_key}"
 
 JUMPBOX_IP=$(terraform state show -state ${TSTATE_FILE} aws_instance.jumpbox | grep public_ip | tail -n 1 | awk '{print $3}')
 ssh -tt -o "StrictHostKeyChecking=no" -i ${PRIVATE_KEY_PATH} ubuntu@${JUMPBOX_IP} PIVNET_TOKEN=${PIVNET_TOKEN} \
@@ -20,6 +22,6 @@ OPSMAN_PASSWORD=${OPSMAN_PASSWORD} \
 'bash -es' <<ENDSSH
   cd management-vpc
   git pull origin master
-  BOSH_WORK_DIR=../bosh_work TEAM_USERNAME=${TEAM_USERNAME} TEAM_PASSWORD=${TEAM_PASSWORD}   AWS_ACCESS_KEY=${AWS_ACCESS_KEY} AWS_SECRET_KEY=${AWS_SECRET_KEY} OPSMAN_PASSWORD=${OPSMAN_PASSWORD} ./concourse/prepare_team.sh
+  BOSH_WORK_DIR=../bosh_work ./concourse/prepare_team.sh
   exit
 ENDSSH
--- destroy_bosh.sh
+++ destroy_bosh.sh
@@ -7,11 +7,11 @@ director_name=$(bosh-cli int ./director.yml --path /director_name)
 export BOSH_CLIENT=admin
 export BOSH_CLIENT_SECRET=$(bosh-cli int ${BOSH_WORK_DIR}/creds.yml --path /admin_password)
 
-bosh-cli -e ${director_name} -d vault delete-deployment
-bosh-cli -e ${director_name} -d concourse delete-deployment
-bosh-cli -e ${director_name} clean-up --all
+bosh-cli -n -e ${director_name} -d vault delete-deployment
+bosh-cli -n -e ${director_name} -d concourse delete-deployment
+bosh-cli -n -e ${director_name} clean-up --all
 
-bosh-cli delete-env bosh-deployment/bosh.yml \
+bosh-cli -n delete-env bosh-deployment/bosh.yml \
   --state ./${BOSH_WORK_DIR}/state.json \
   -o bosh-deployment/aws/cpi.yml \
   --vars-store ${BOSH_WORK_DIR}/creds.yml \
