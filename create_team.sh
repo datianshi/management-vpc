87b654b8ed28716ac256317fe3ebeb34626187ca
datianshi <dsz0111@gmail.com>
2017-10-22 10:14:06 -0500

    test

--- create_team.sh
+++ create_team.sh
@@ -4,7 +4,7 @@
 
 # PCF_ENV=test TEAM_USERNAME=test TEAM_PASSWORD=test OPSMAN_PASSWORD=admin ./create_team.sh
 
-set -e
+set -ex
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 TSTATE_FILE="${DIR}/terraform.tfstate"
@@ -19,6 +19,7 @@ TEAM_PASSWORD=${TEAM_PASSWORD} \
 AWS_ACCESS_KEY=${TF_VAR_aws_access_key} \
 AWS_SECRET_KEY=${TF_VAR_aws_secret_key} \
 OPSMAN_PASSWORD=${OPSMAN_PASSWORD} \
+"KEY_PEM=${KEY_PEM}" \
 'bash -es' <<ENDSSH
   cd management-vpc
   git pull origin master
